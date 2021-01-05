14215a7bc1b3315d3e5be8f43218725ed77dac73
Massimo Scamarcia <massimo.scamarcia@gmail.com>
2021-01-05 21:08:05 +0100

    Initial import.

---
+++ .envs/.local/.app
@@ -0,0 +1,3 @@
+DATABASE_URL=mysql://appuser:apppassword@database:3306/symfony?serverVersion=mariadb-10.5.8
+APP_ENV=dev
+APP_SECRET=24e17c47430bd2044a61c131c1cf6990
---
+++ docker/php-fpm/docker_entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $1 == 'serve' ]] ; then
+
+    composer install
+
+    if [[ $2 == 'dev' ]] ; then
+
+        wait-for-it database:3306 -- bin/console doctrine:schema:update
+        bin/console doctrine:fixtures:load
+        bin/console doctrine:migrations:migrate --allow-no-migration
+
+    elif [[ $2 == 'prod' ]] ; then
+
+        APP_ENV=prod
+
+    fi
+
+    php-fpm
+
+else
+    exec "$@"
+fi
